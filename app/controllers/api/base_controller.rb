e8b01011d9ffaf0bf1d70aff62575dd4e2403de1
Le Vady <levady.sinarta@gmail.com>
2020-12-08 19:30:05 +0800

    add basic authentication to the API

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,10 +1,21 @@
 module Api
   class BaseController < ActionController::Base
+    before_action :basic_authenticate
+
     rescue_from ActiveRecord::RecordNotFound do
       head :not_found
     end
 
+    delegate :secure_compare, to: ActiveSupport::SecurityUtils
+
     private
+
+    def basic_authenticate
+      authenticated = authenticate_with_http_basic do |u, p| 
+        secure_compare(u, ENV["BASIC_AUTH_USER"].to_s) && secure_compare(p, ENV["BASIC_AUTH_PASSWORD"].to_s)
+      end
+      request_http_basic_authentication unless authenticated
+    end
 
     def render_validation_error(errors)
       render json: { errors: errors }, status: :unprocessable_entity
--- spec/requests/api/v1/visitors_controller_spec.rb
+++ spec/requests/api/v1/visitors_controller_spec.rb
@@ -7,26 +7,49 @@ RSpec.describe Api::V1::VisitorsController, type: :request do
   let!(:ride) { create(:ride) }
   
   describe "POST create" do
-    it "return 200 OK on success" do
-      payload = { visitor: { sensor_code: ride.sensor_code, visitors_count: 2, collected_at: current_time }}
-      post api_v1_visitors_path(format: :json), params: payload
-      expect(response.status).to eq(200)
+    context "basic authenticated" do
+      let(:auth) do
+        ActionController::HttpAuthentication::Basic.encode_credentials("username", "secure")
+      end
+      let(:headers) { { "HTTP_AUTHORIZATION" => auth } }
+
+      around do |example|
+        ENV["BASIC_AUTH_USER"] = "username"
+        ENV["BASIC_AUTH_PASSWORD"] = "secure"
+        example.run
+        ENV["BASIC_AUTH_USER"] = nil
+        ENV["BASIC_AUTH_PASSWORD"] = nil
+      end
+
+      it "return 200 OK on success" do
+        payload = { visitor: { sensor_code: ride.sensor_code, visitors_count: 2, collected_at: current_time }}
+        post api_v1_visitors_path(format: :json), params: payload, headers: headers
+        expect(response.status).to eq(200)
+      end
+  
+      it "return internal server error when ride does not exist" do
+        payload = { visitor: { sensor_code: "foobar", visitors_count: 2, collected_at: current_time }}
+        post api_v1_visitors_path(format: :json), params: payload, headers: headers
+        expect(response.status).to eq(500)
+      end
+  
+      it "return JSON error response for validation errors" do
+        payload = { visitor: { sensor_code: nil, visitors_count: nil, collected_at: nil }}
+        
+        post api_v1_visitors_path(format: :json), params: payload, headers: headers
+        
+        expect(response.status).to eq(422)
+        json = JSON.parse(response.body)
+        expect(json).to eq({"errors"=>"Validation failed: Sensor code can't be blank, Visitors count can't be blank, Collected at can't be blank"})
+      end
     end
 
-    it "return internal server error when ride does not exist" do
-      payload = { visitor: { sensor_code: "foobar", visitors_count: 2, collected_at: current_time }}
-      post api_v1_visitors_path(format: :json), params: payload
-      expect(response.status).to eq(500)
-    end
-
-    it "return JSON error response for validation errors" do
-      payload = { visitor: { sensor_code: nil, visitors_count: nil, collected_at: nil }}
-      
-      post api_v1_visitors_path(format: :json), params: payload
-      
-      expect(response.status).to eq(422)
-      json = JSON.parse(response.body)
-      expect(json).to eq({"errors"=>"Validation failed: Sensor code can't be blank, Visitors count can't be blank, Collected at can't be blank"})
+    context "unauthenticated" do
+      it "return 401 unauthorized" do
+        payload = { visitor: { sensor_code: ride.sensor_code, visitors_count: 2, collected_at: current_time }}
+        post api_v1_visitors_path(format: :json), params: payload, headers: headers
+        expect(response.status).to eq(401)
+      end
     end
   end
 end
