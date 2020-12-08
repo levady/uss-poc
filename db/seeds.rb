db7f5bfc927bca04b0fb2178f7f98b6f6b2d74ad
Le Vady <levady.sinarta@gmail.com>
2020-12-08 22:20:50 +0800

    add visitors seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,21 @@
 
 # Since rides data can be static, seeding them here to populate them
 Ride.create([
-  { name: "Transformers: The Ride 3D", capacity: 32, duration: 4.5, sensor_code: "ride-1" },
-  { name: "Battlestar Galactica", capacity: 40, duration: 3, sensor_code: "ride-2" },
+  { name: "Transformers: The Ride 3D", duration: 3, capacity: 6, sensor_code: "ride-1" },
+  { name: "Battlestar Galactica", duration: 7, capacity: 30, sensor_code: "ride-2" },
   { name: "Revenge of the Mummy", capacity: 18, duration: 5, sensor_code: "ride-3" }
+])
+
+collection_time = Time.now
+ride1 = Ride.find_by(sensor_code: "ride-1")
+ride2 = Ride.find_by(sensor_code: "ride-2")
+Visitor.create([
+  { ride: ride1, visitors_count: 20, collected_at: collection_time },
+  { ride: ride1, visitors_count: 12, collected_at: collection_time + 3.minutes },
+  { ride: ride1, visitors_count: -4, collected_at: collection_time + 1.minute },
+  { ride: ride2, visitors_count: 50, collected_at: collection_time },
+  { ride: ride2, visitors_count: -11, collected_at: collection_time + 1.minute },
+  { ride: ride2, visitors_count: 40, collected_at: collection_time + 7.minute },
+  { ride: ride1, visitors_count: 50, collected_at: collection_time - 1.day },
+  { ride: ride2, visitors_count: -11, collected_at: collection_time + 1.day }
 ])
