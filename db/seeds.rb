ae3ed58d0dcf6fba3df8960d53b0d60fb8ef1761
Le Vady <levady.sinarta@gmail.com>
2020-12-08 14:12:41 +0800

    seed rides data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Since rides data can be static, seeding them here to populate them
+Ride.create([
+  { name: "Transformers: The Ride 3D", capacity: 32, duration: 4.5, sensor_code: "ride-1" },
+  { name: "Battlestar Galactica", capacity: 40, duration: 3, sensor_code: "ride-2" },
+  { name: "Revenge of the Mummy", capacity: 18, duration: 5, sensor_code: "ride-3" }
+])
